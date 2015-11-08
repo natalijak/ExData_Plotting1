8ee129f89e0aca94ee3ef5aa22599274125f10e0
natalijak <fru@sunrise.ch>
2015-11-08 04:09:41 +0100

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,48 @@
+#getwd()
+#setwd()
+
+#CODE FOR THE DATA LOAD, DATA MANIPULATION dataset gets loaded, and various data checks are performed (Data formating, Data subsetting)
+##Step 1: download the zip file   => Dataset: Electric power consumption [20Mb]
+##Step 2. read the csv file, caution NA values
+
+hhpc_data <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+#check summary
+summary(hhpc_data)
+#check dimensions
+dim(hhpc_data)
+#result : [1] 2075259       9  / correct
+
+#validate if dataset consist of columns as stated in the introduction
+names(hhpc_data)
+##result: [1] "Date"  "Time"  "Global_active_power"  "Global_reactive_power" "Voltage"  "Global_intensity"  "Sub_metering_1"  "Sub_metering_2"  "Sub_metering_3"     
+##correct, all fine
+
+#format date
+hhpc_data$Date <- as.Date(hhpc_data$Date, format="%d/%m/%Y")  #initial format  16/12/2006  new format 2006-12-16
+#check top 
+head(hhpc_data)
+#check tail
+tail(hhpc_data)
+#okay, proceed further
+
+## subsetting the dataset to following timerange: 2007-02-01   and 2007-02-02
+hhpc_subset <- subset(hhpc_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+dim(hhpc_subset) #result , dataset to work with for the assignment [1] 2880    9 
+
+#******************************************************************************************#
+# CODE FOR PLOTTING THE SUBSETTED DATASET
+## Requirements: Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+##Name each of the plot files as plot1.png, plot2.png ...
+
+## Plot 1 "Global Active Power" Frequency
+hist(hhpc_subset$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png, file="plot1.png", height=480, width=480)  #png 4
+dev.off()   #RStudioGD 2 
+
+
+#END CODE#
+
